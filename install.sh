539e8c3d74e8727dcf05b5639b2d564ea734ba4e
Jan <jan@trick77.com>
2019-12-31 10:50:36 +0100

    Installer fixed.

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# Installs additional LogCheck filter rules using symlinks from a git repo.
+# Installs additional LogCheck filter rules.
 #
 set -euo pipefail
 
@@ -15,6 +15,6 @@ local_files=(
 for local_file in "${local_files[@]}"
 do
   destname=${logcheck_path}/${local_file}
-  echo "Creating symlink for ${local_file} as ${destname}"
-  ln -s ${PWD}/${local_file} ${destname}
+  echo "Copying ${local_file} to ${destname}"
+  cp ${local_file} ${destname}
 done
