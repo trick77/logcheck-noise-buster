e6aabdf53f2a4ad74287d1f9eeaabbed1921014b
Jan <jan@trick77.com>
2020-01-18 08:41:14 +0100

    Tests improved.

--- test/test.sh
+++ test/test.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 set -euxo pipefail
-
-logcheck-test -q --log-file ./syslog --rule-file ../ignore.d.server/local-systemd
-logcheck-test -q --log-file ./auth.log --rule-file ../ignore.d.server/local-sshd
+logcheck-test --log-file ./syslog --rule-file ../ignore.d.server/local-systemd
+logcheck-test --log-file ./auth.log --rule-file ../ignore.d.server/local-sshd
