f4054042ef95e4161b7e167af08c336e81733865
trick77 <jan@trick77.com>
2022-11-27 16:45:37 +0100

    Improve script

--- debian-10-buster/install.sh
+++ debian-10-buster/install.sh
@@ -19,5 +19,5 @@ for local_file in "${local_files[@]}"
 do
   destname=${logcheck_path}/${local_file}
   echo "Copying ${local_file} to ${destname}"
-  cp ${local_file} ${destname}
+  cp "${local_file}" "${destname}"
 done
