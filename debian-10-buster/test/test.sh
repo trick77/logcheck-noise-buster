4728fd068a0c4ae5533dc311a45508fca63ae759
trick77 <jan@trick77.com>
2025-11-23 09:16:45 +0100

    Remove outdated versions

--- debian-10-buster/test/test.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -euxo pipefail
-logcheck-test --log-file ./syslog --rule-file ../ignore.d.server/local-systemd
-logcheck-test --log-file ./auth.log --rule-file ../ignore.d.server/local-sshd
