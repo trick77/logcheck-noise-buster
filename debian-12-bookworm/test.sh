eafff34feeeb6f81fcc89c02dd65775361d8606f
trick77 <1525566+trick77@users.noreply.github.com>
2025-12-24 13:44:12 +0000

    chore: auto-fix lint issues

--- debian-12-bookworm/test.sh
+++ debian-12-bookworm/test.sh
@@ -6,18 +6,18 @@ GREEN="\e[92m"
 RESET="\e[0m"
 
 for file in test/*; do
-	NAME=$(basename "$file")
-	echo -n "Testing $NAME ... "
-	LOG_LINES=$(logcheck-test -q -i -l "$file" -r "ignore.d.server/$NAME")
-	RESULT=$?
-	LOG_COUNT=$(echo -n "$LOG_LINES" | wc -l)
-	if [ "$RESULT" -ne 1 ] || [ "$LOG_COUNT" -ne 0 ]; then
-		echo -e "${RED}FAILED${RESET}"
-		echo "$LOG_LINES"
-		FAIL=$((FAIL + 1))
-	else
-		echo -e "${GREEN}OK${RESET}"
-	fi
+  NAME=$(basename "$file")
+  echo -n "Testing $NAME ... "
+  LOG_LINES=$(logcheck-test -q -i -l "$file" -r "ignore.d.server/$NAME")
+  RESULT=$?
+  LOG_COUNT=$(echo -n "$LOG_LINES" | wc -l)
+  if [ "$RESULT" -ne 1 ] || [ "$LOG_COUNT" -ne 0 ]; then
+    echo -e "${RED}FAILED${RESET}"
+    echo "$LOG_LINES"
+    FAIL=$((FAIL + 1))
+  else
+    echo -e "${GREEN}OK${RESET}"
+  fi
 done
 
 exit $FAIL
