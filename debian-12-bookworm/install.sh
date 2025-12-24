4c02c2949eb53cb8ee1bc5e9621e3a8a9a230f51
trick77 <jan@trick77.com>
2025-12-24 11:17:28 +0100

    Reformat scripts

--- debian-12-bookworm/install.sh
+++ debian-12-bookworm/install.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
-#
-# Installs additional LogCheck filter rules.
-#
+
 set -euo pipefail
 
-#!/bin/sh
-set -e
 cd ./ignore.d.server
 for name in * ; do
-    path="/etc/logcheck/ignore.d.server/local-$name"
-    echo "Symlinking ${name} to ${path}"
-    if [ ! -f "${path}" ] ; then
-        ln -s "$(readlink -f "${name}")" "${path}"
-    fi
+  path="/etc/logcheck/ignore.d.server/local-$name"
+  echo "Symlinking ${name} to ${path}"
+  if [ ! -f "${path}" ] ; then
+    ln -s "$(readlink -f "${name}")" "${path}"
+  fi
 done
